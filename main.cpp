e23c91fc82f130c8466a23f0b0c9f8a7b9118948
gianabif14 <gianabifirdaus14@gmail.com>
2025-05-15 10:33:07 +0700

    chrono off
    karena laptopku gabisa

--- main.cpp
+++ main.cpp
@@ -1009,7 +1009,7 @@ void cariBuku()
             cout << setfill('-') << setw(40) << " " << endl;
             cout << endl;
 
-            this_thread::sleep_for(chrono::seconds(1));
+            //this_thread::sleep_for(chrono::seconds(1));
         }
     }
     if (!adaYgKetemu)
